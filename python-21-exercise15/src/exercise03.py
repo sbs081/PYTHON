599fefe15483ed0a1d79c3f79c5fb18f5ad6499b
sbs081 <aronivicki@gmail.com>
2015-12-22 15:33:03 +0800

    Update exercise03.py

--- python-21-exercise15/src/exercise03.py
+++ python-21-exercise15/src/exercise03.py
@@ -6,5 +6,5 @@ Created on 2009-10-23
 import re
 
 if __name__ == '__main__':
-    pattern = re.compile(r'[a-zA-Z], \b[a-zA-Z]+\b')
+    pattern = re.compile(r'[a-zA-Z], \b[a-zA-Z0-9]+\b')
     print pattern.search('M, tom1').group()
